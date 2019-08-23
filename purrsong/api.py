3fc3c687ed4425b08b67d9661d809b242acef0dc
jeakwon <onlytojay@gmail.com>
2019-08-24 02:20:53 +0900

    catfacedetector lmks added

--- purrsong/api.py
+++ purrsong/api.py
@@ -2,3 +2,4 @@ from purrsong.datasets import load as load_dataset
 from purrsong.datasets import list_datasets
 from purrsong.models import load as load_model
 from purrsong.models import list_models
+from purrsong.utils.path import *
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import purrsong as ps
-from purrsong.utils.imagetools import CatFaceDetector
+
+from purrsong.apps.catface import CatFaceDetector
 
 if __name__ == "__main__":
-    pass
-
-    
-    # ps.list_datasets()
+    cfd=CatFaceDetector(bbs_model='bbs', lmks_model='lmks')
+    imagedir = ps.join(ps.load_dataset('cats'), 'cats', 'CAT_00')
+    jpgs = ps.listext(imagedir, ['jpg'])
+    for jpg in jpgs:
+        cfd.extract(ps.join(imagedir, jpg), ps.join(ps.DESKTOP, 'lmks', jpg+'.cat'))
     # ps.list_models()
