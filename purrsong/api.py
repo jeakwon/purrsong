b320e5d548eae8eb91c29e65b4bb50503aed2317
jeakwon <onlytojay@gmail.com>
2019-08-26 04:42:10 +0900

    updated api.py

--- purrsong/api.py
+++ purrsong/api.py
@@ -1,6 +1,7 @@
 from purrsong.datasets.core import load as load_dataset
 from purrsong.datasets.core import list_datasets
 from purrsong.datasets.cats import Cats
+from purrsong.datasets.cats import load_cats
 from purrsong.models.core import load as load_model
 from purrsong.models.core import list_models
 from purrsong.utils.path import *
