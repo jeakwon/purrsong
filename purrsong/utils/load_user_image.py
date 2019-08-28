ef6ffeb22c2ea2b55af3c82e511b5a3a21ce976c
chongs02 <chongs02@naver.com>
2019-08-28 09:25:53 +0900

    update load_user_image func

--- purrsong/utils/load_user_image.py
+++ purrsong/utils/load_user_image.py
@@ -1,7 +1,7 @@
 
 import cv2
 
-def load_user_image(image):
+def load_user_image(image_path):
     """load_user_image
 
        @Load User Image
@@ -12,7 +12,7 @@ def load_user_image(image):
        :return: RGB image
        :type return: np.array
      """
-    img = cv2.imread(image, cv2.IMREAD_COLOR)
+    img = cv2.imread(image_path, cv2.IMREAD_COLOR)
     # convert BGR to RGB
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
